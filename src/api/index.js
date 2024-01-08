53b16804018c2d312af5caaffa8dda066f849500
suman kumar <43146870+achuara@users.noreply.github.com>
2024-01-08 11:19:12 +0530

    Update backend url index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({baseURL: 'https://sharepic-server.onrender.com'});
+const API = axios.create({baseURL: 'https://vercel-server-theta-three.vercel.app'});
 
 API.interceptors.request.use( (req) => {
     if(localStorage.getItem('profile')){
