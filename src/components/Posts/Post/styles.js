01a47a80a62f86d99997d5c9b0bbe89db0292714
suman kumar <43146870+achuara@users.noreply.github.com>
2024-01-21 21:41:45 +0530

    style: Update styles.js height  of image

--- src/components/Posts/Post/styles.js
+++ src/components/Posts/Post/styles.js
@@ -2,7 +2,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles({
   media: {
-    height: '100%',
+    height: 100,
     paddingTop: '56.25%',
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
     
